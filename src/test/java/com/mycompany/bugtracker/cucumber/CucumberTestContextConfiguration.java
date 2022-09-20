d4003db905a4a22b5eef70767e270ca1bb57572e
Thomas Abecassis <stingerkiller34@gmail.comls>
2022-09-20 14:55:37 +0000

    Initial version of BugTrackerJHipster generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/mycompany/bugtracker/cucumber/CucumberTestContextConfiguration.java
@@ -0,0 +1,10 @@
+package com.mycompany.bugtracker.cucumber;
+
+import com.mycompany.bugtracker.IntegrationTest;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@CucumberContextConfiguration
+@IntegrationTest
+@WebAppConfiguration
+public class CucumberTestContextConfiguration {}
